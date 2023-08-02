4483cfc04e0096f9ded1ffba5d52168a929b13a1
melqrozendo <78502819+melqrozendo@users.noreply.github.com>
2023-08-02 18:25:24 -0300

    commit 01

---
+++ script.js
@@ -0,0 +1,16 @@
+/*function mudar(){
+
+var data = new Date()
+
+var hora = data.getHours()
+//var hora = 1
+
+if(hora >= 0 && hora < 12){
+    document.body.style.background = '#772F1A'
+} else if(hora >= 12 && hora < 18){
+    document.body.style.background = '#1A512E'
+} else {
+    document.body.style.background = '#28313B'
+}
+
+}*/
